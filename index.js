da07fd373d0f4fe4b6b9bb8f545ebf44cdc53b10
RamiZregat <rami.zregat97@gmail.com>
2021-12-29 16:38:53 +0200

    done

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+const PORT=process.env.PORT || 3002;
 
-const { start } = require('./app');
-const PORT =process.env.PORT || 3002;
-  start(PORT); 
+const server = http.createServer(app);
+
+// let array=[];
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  // console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    // array.push(data)
+    // console.log(array);
+
+    // console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    // console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("SERVER RUNNING");
+});
+
+
